c85b2b03c3f821c51e20a42496d83e966bea0618
Radiofisik <mail@radiofisik.ru>
2014-09-09 22:16:06 +0300

    uninstaller added

--- uninstall.php
+++ uninstall.php
@@ -1,4 +1,19 @@
 <?php
 if(!defined('WP_UNINSTALL_PLUGIN')) exit;
 
+global $wpdb;
+	//create orders table
+	$tablename= $wpdb->prefix."orders";
+		if($wpdb->get_var("show tables like '$tablename'")==$tablename)
+		{
+		$sql="DROP TABLE `$tablename`";
+		$wpdb->query($sql);
+		}
+		//create items table
+		$tablename= $wpdb->prefix."items";
+		if($wpdb->get_var("show tables like '$tablename'")==$tablename)
+		{
+		$sql="DROP TABLE `$tablename`";
+		$wpdb->query($sql);
+		}
 ?>

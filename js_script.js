94f68bce027a6c13ec8b68a61b26ca4493a3f663
Radiofisik <mail@radiofisik.ru>
2014-09-11 19:33:51 +0300

    jQuery( document ) added
    jQuery should be used instead of $

--- js_script.js
+++ js_script.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {
+jQuery( document ).ready(function() {
   // Handler for .ready() called.
  
  $('.addbtn').click(function(){
@@ -8,8 +8,7 @@ $( document ).ready(function() {
 	var a=$('[name*="Quantity"]:last').attr("name")+"1";
 	$('[name*="Quantity"]:last').attr("name",a);
 	var b=$('[name*="Product"]:last').attr("name")+"1";
-	$('[name*="Product"]:last').attr("name",b);
-	
+	$('[name*="Product"]:last').attr("name",b);	
 }); 
   
 $('#mainform').submit(function()
